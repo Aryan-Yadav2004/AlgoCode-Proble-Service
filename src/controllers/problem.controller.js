0c629b48f95ee0dbacaa70830dff0038b8602775
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-12-04 19:39:46 +0530

    Feat: add routing structure for versioned api for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,26 @@
+
+function pingProblemController(req,res) {
+    return res.json({message: 'Ping controller is up'});
+}
+
+function addProblem(req, res){
+
+}
+
+function getProblem(req, res) {
+
+}
+
+function getProblems(req, res) {
+
+}
+
+function deleteProblem(req, res) {
+
+}
+
+function updateProblem(req, res) {
+
+}
+
+export { pingProblemController, addProblem, getProblem, getProblems, deleteProblem, updateProblem }
