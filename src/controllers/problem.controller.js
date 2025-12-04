29eb1b735cf584c2cb380440c29f64ff63aca52a
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-12-04 20:25:11 +0530

    Status code added

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,26 +1,26 @@
-
+import { StatusCodes } from  'http-status-codes'
 function pingProblemController(req,res) {
     return res.json({message: 'Ping controller is up'});
 }
 
 function addProblem(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not implemented'});
 }
 
 function getProblem(req, res) {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not implemented'});
 }
 
 function getProblems(req, res) {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not implemented'});
 }
 
 function deleteProblem(req, res) {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not implemented'});
 }
 
 function updateProblem(req, res) {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not implemented'});
 }
 
 export { pingProblemController, addProblem, getProblem, getProblems, deleteProblem, updateProblem }
