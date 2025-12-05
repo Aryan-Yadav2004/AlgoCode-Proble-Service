9f7d0cb8c49504484b69f5b0271693914cc5e8a8
Aryan-Yadav2004 <yadavaryan122004@gmail.com>
2025-12-05 20:40:43 +0530

    Feat adding error handling

---
+++ src/utils/errorHandler.js
@@ -0,0 +1,21 @@
+import BaseError from "../errors/base.error.js";
+import { StatusCodes } from "http-status-codes";
+function errorHandler(err, req, res ,next) { 
+    if(err instanceof BaseError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            error: err.details,
+            data: {} // because this is an exception so no data is going to be provided
+        }); 
+    }
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: "Something went wrong",
+        error: err,
+        data: {} // because this is an exception so no data is going to be provide
+    });
+}
+
+export default errorHandler;
